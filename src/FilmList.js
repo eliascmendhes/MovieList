bcce7da8aa333e8a67d9d761a196d6b7a7c17f8a
Elias <45702980+eliascmendhes@users.noreply.github.com>
2023-07-21 13:18:30 -0300

    Update FilmList.js

--- src/FilmList.js
+++ src/FilmList.js
@@ -7,7 +7,7 @@ function FilmList() {
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=66ce6f59fe7c4a88f26f5b9b43af1ca3&language=pt-br-US&page=1"
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=language=pt-br-US&page=1"
       );
       const data = await response.json();
       setMovies(data.results);
